7d374bbf6af2c013060d884efb309ff4942079d4
ami0131 <amirlan0131@gmail.com>
2024-03-06 09:13:46 +0800

    aa

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Pool } from "pg";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+export default async function handler(req, res) {
+  const client = await pool.connect();
+
+  try {
+    const response = await client.query(
+      "CREATE TABLE users (ID int NOT NULL, email VARCHAR(50) UNIQUE NOT NULL, name VARCHAR(50) NOT NULL, password text, avatar_img varchar, createdAt TIMESTAMP,updatedAt TIMESTAMP, currency_type TEXT DEFAULT 'MNT');"
+    );
+
+    console.log(response.rows[0]);
+
+    res.status(200).json();
+  } finally {
+    client.release();
+  }
 }
